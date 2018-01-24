bc826d94a13734c0b5d174062bf3a35506f9702a
Daniel Santos <dfsantosbu@unal.edu.co>
2018-01-24 11:40:58 -0500

    Modularize application.

--- detec_color.py
+++ detec_color.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-import cv2
-import numpy as np
 import argparse
+import cv2
+from logic import start
 
 __author__ = "Daniel Fernando Santos Bustos"
 __license__ = "MIT"
@@ -17,23 +17,6 @@ arguments  = vars(arguments_parser.parse_args())
 
 #load the image
 image = cv2.imread(arguments["image"])
-
-#Now define the boundaries for the colors that we want to capture
-boundaries = [
- ([0, 0, 0], [255,50, 50]), #blue
- ([0, 0, 0], [50,255, 50]), #green
- ([0, 0, 0], [50,50, 255]), #red
-]
-
-
-def start(image):
-    for (lower, upper) in boundaries:
-    	lower = np.array(lower, dtype = "uint8")
-    	upper = np.array(upper, dtype = "uint8")
-    	mask = cv2.inRange(image, lower, upper)
-    	output = cv2.bitwise_and(image, image, mask = mask)
-    	cv2.imshow("images", np.hstack([image, output]))
-    	cv2.waitKey(0)
 
 if __name__ == "__main__":
     start(image)
